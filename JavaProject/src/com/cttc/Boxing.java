74382810f2505d2fe8945fe2cd744dec398ebfb7
CTTC <CTTC@DESKTOP-PSFK2SM>
2024-10-08 08:09:40 +0530

    Boxing and Un-boxing

---
+++ JavaProject/src/com/cttc/Boxing.java
@@ -0,0 +1,16 @@
+package com.cttc;
+
+public class Boxing {
+
+	public static void main(String[] args) {
+		String a = "10";
+		String b = "20";
+		int i = Integer.parseInt(a);
+		int j = Integer.parseInt(b);
+		int c = i + j;
+		System.out.println(c);
+		
+		String r = Integer.toString(c);
+		System.out.println(r);
+	}
+}
